68acdee412aacf2c5fa03cfc1703914b492a37a9
Denis Zhmurenko <d.zhmurenko@dodobrands.io>
2025-01-27 16:37:18 +0000

    add some logs

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,23 @@ const server = createServer({
 const clients: Record<string, string> = {};
 
 aedes.authenticate = (client, username, password, callback) => {
-    if(username !== MQTT_USERNAME || !password) return callback(new AuthenticateError(4), null);
+    if(username !== MQTT_USERNAME || !password) {
+        console.warn('Failed auth (incorrect username or no password)', client.id);
+        return callback(new AuthenticateError(4), null);
+    }
     callback(null, true);
     clients[client.id] = Buffer.from(password).toString('base64');
+    console.log('Successful auth', client.id);
 }
 
 aedes.authorizePublish = async (client, packet, callback) => {
     if (packet.topic.startsWith('$SYS')) {
+        console.warn('Rejected publish to SYS topic');
         return callback(new Error('Sys is reserved'))
     }
 
     if(!client || !clients[client.id]) {
+        console.error('No token found on file while authorizing publish')
         return callback(new Error('No token on file for publishing client'));
     }
 
@@ -40,8 +46,9 @@ aedes.authorizePublish = async (client, packet, callback) => {
             )
         );
         callback(null);
+        console.log('Successful reading post', client.id);
     } catch (e) {
-        console.error('error sending reading', e);
+        console.error('Error sending reading', e);
         callback(e as Error);
     }
 }
